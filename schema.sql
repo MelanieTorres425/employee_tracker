83f73240b6322ad76a0adf38bd0d2d9c5a6b995d
Bugs <melanietorres425@live.com>
2020-09-11 22:38:19 -0400

    added to sql & seeds

--- schema.sql
+++ schema.sql
@@ -3,26 +3,35 @@ CREATE database employee_trackerDB;
 
 USE employee_trackerDB;
 
+DROP TABLE IF EXISTS department;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NULL,
-  salary DECIMAL(10,4) NULL,
-  department_id INT NULL,
-  PRIMARY KEY (id)
-);
+	DROP TABLE IF EXISTS role;
+	CREATE TABLE role (
+	  id INT NOT NULL AUTO_INCREMENT,
+	  title VARCHAR(30) NULL,
+	  salary DECIMAL(8,2) NULL,
+	  department_id INT NULL,
+	  PRIMARY KEY (id),
+	  FOREIGN KEY (department_id) REFERENCES department (id)
+	);
 
-CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
-  role_id INT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id)
-);
+	DROP TABLE IF EXISTS employee;
+	CREATE TABLE employee (
+	  id INT NOT NULL AUTO_INCREMENT,
+	  first_name VARCHAR(30) NULL,
+	  last_name VARCHAR(30) NULL,
+	  role_id INT NULL,
+	  manager_id INT NULL,
+	  PRIMARY KEY (id),
+      FOREIGN KEY (role_id) REFERENCES role (id),
+      FOREIGN KEY (manager_id) REFERENCES employee (id)
+      
+	);
+
+
 
